0c0b2ba756bfc09edd224e501c0894828eb0ac0e
Edgar Vega <vayne.edgar271@gmail.com>
2021-07-31 14:32:06 -0500

    fix issue from mikro-orm.config

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -2,10 +2,11 @@ import { Options } from "@mikro-orm/core";
 import { MongoHighlighter } from "@mikro-orm/mongo-highlighter";
 import { MongoDriver } from "@mikro-orm/mongodb";
 import { TsMorphMetadataProvider } from "@mikro-orm/reflection";
+import { config } from "./config";
 
 const mikroOrmMongoConfig: Options<MongoDriver> = {
     type: 'mongo',
-    clientUrl: 'mongodb://root:root@mongodb:27017',
+    clientUrl: `mongodb://${config.database.username}:${config.database.password}@${config.database.host}:${config.database.port}`,
     dbName: 'challenge',
     entities: [],
     discovery: {warnWhenNoEntities: false},
